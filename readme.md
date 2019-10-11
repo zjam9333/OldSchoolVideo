268236c5b67acd09c66b17928e98e0617cdbd07b
ZJam <670231925@qq.com>
2019-10-11 19:03:38 +0800

    fix some purple edge

--- readme.md
+++ readme.md
@@ -1,12 +1,11 @@
-# 将视频转换为复古的VHS风格
-## 需要opencv、ffmpeg
-## python3 vhs.py -i input.mp4 -o output.mp4
+# 将视频转换为复古风格
+
+## 需要opencv-python、ffmpeg
+
+## python vhs.py -i input.mp4 -o output.mp4
 
 ### input 
 ![input image](test.jpg)
 
 ### output
-![output image](out.jpg)
-
-### interlace
-![interlace demo](interlace.jpg)
+![output my style](output_mystyle.jpg)
--- vhs.py
+++ vhs.py
@@ -5,79 +5,55 @@ import numpy as np
 import os
 import time
 import argparse
+from tqdm import tqdm
 
+def purpleEdge(src, size = 1):
+    copy = src.copy()
+    # [BGR] [012]
+    # 2, 0:2 cyan red (tiktok)
+    # 1, 0::2 purple green (normal)
+    # 0, 1: yello blue (unknown)
+    channel = copy[:, :, 1]
+    mov = size
+    newchannel = cv2.resize(channel, (channel.shape[1] + mov * 2, channel.shape[0] + mov * 2))
+    channel[:, :] = newchannel[mov:-mov, mov:-mov]
+    return copy
+
+# sharpen
+sharpkernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
 def MyStyle(src):
     copy = src.copy()
+
     # blur
     blursize = 3
     copy = cv2.GaussianBlur(copy, (blursize, blursize), 0)
-    # sharpen 
-    kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
-    copy = cv2.filter2D(copy, -1, kernel)
 
-    # change Saturation
+    # # make more yellow
+    B = copy[:, :, 0]
+    B[:, :] = B[:, :] * 0.95
+
+    # # change Saturation
     HSV = cv2.cvtColor(copy, cv2.COLOR_BGR2HSV)
     S = HSV[:, :, 1]
-    S[:, :] = S[:, :] * 0.8
+    HSV[:, :, 1] = S * 0.8
     copy = cv2.cvtColor(HSV, cv2.COLOR_HSV2BGR)
 
     # translate color's channel
-    move = 1
-    cols = copy.shape[1]
-    # pick one channel to translate, 0:B 1:G 2:R
-    toLeft = copy[:, :, 0]
-    toLeft[:, :cols - move] = toLeft[:, move:]
-
-    toRight = copy[:, :, 2]
-    toRight[:, move:] = toRight[:, :cols - move]
-
-    rows = copy.shape[0]
-    toUp = copy[:, :, 1]
-    toUp[:rows - move:] = toUp[move:, :]
-
-    # make more yellow
-    B = copy[:, :, 0]
-    B[:, :] = B[:, :] * 0.94
-    G = copy[:, :, 1]
-    G[:, :] = G[:, :] * 0.99
-    return copy
-
-def VXStyle(src):
-    copy = src.copy()
-
-    # make more yellow
-    B = copy[:, :, 0]
-    B[:, :] = B[:, :] * 0.9
-
-    # vx ?
-    # blur
-    blursize = 5
-    copy = cv2.GaussianBlur(copy, (blursize, blursize), 0)
-
-    # change Saturation
-    HSV = cv2.cvtColor(copy, cv2.COLOR_BGR2HSV)
-    S = HSV[:, :, 1]
-    S[:, :] = S[:, :] * 0.65
-    # make brighter
-    Vfloat = HSV[:, :, 2].astype(np.int)
-    Vfloat[:] = Vfloat[:] * 1.5
-    Vfloat[Vfloat > 255] = 255
-    HSV[:, :, 2] = Vfloat
-
-    copy = cv2.cvtColor(HSV, cv2.COLOR_HSV2BGR)
-
+    copy = purpleEdge(copy, size = 2)
+    # sharpen 
+    copy = cv2.filter2D(copy, -1, sharpkernel)
     return copy
 
 def HandleImage(src):
-    # return MyStyle(src)
-    return VXStyle(src)    
+    return MyStyle(src) 
+    # return purpleEdge(src)  
 
 def doWithVideo(src, output = 'out.mp4', encodewith264 = True, framepersecond = 30, interlaced = True, perferheight = 720):
     cap = cv2.VideoCapture(src)
     width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
     height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
     fps = cap.get(cv2.CAP_PROP_FPS)
-    framecount = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    framecount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     print('total frame: {}'.format(framecount))
 
     if not os.path.exists(src):
@@ -111,9 +87,10 @@ def doWithVideo(src, output = 'out.mp4', encodewith264 = True, framepersecond = 
         print('fail to open video writer')
         return False
 
-    currentframeindex = 0
+    # currentframeindex = 0
     lastframe = np.array([])
-    while cap.isOpened():
+
+    for _ in tqdm(range(framecount)):
         ret, frame = cap.read()
         if ret == False:
             break
@@ -138,8 +115,8 @@ def doWithVideo(src, output = 'out.mp4', encodewith264 = True, framepersecond = 
             lastframe = frame
         else:
             out.write(frame)
-        print('progress:{}/{}'.format(currentframeindex, framecount))
-        currentframeindex += 1
+        # print('progress:{}/{}'.format(currentframeindex, framecount))
+        # currentframeindex += 1
     cap.release()
     out.release()
     print('finish progress')
@@ -160,11 +137,11 @@ def doWithVideo(src, output = 'out.mp4', encodewith264 = True, framepersecond = 
 def doWithImage(src, output = 'out.jpg'):
     print("progressing")
     img = cv2.imread(src)
-    row = img.shape[0]
-    col = img.shape[1]
-    width = 360
-    height = int(width / col * row)
-    img = cv2.resize(img, (width, height))
+    # row = img.shape[0]
+    # col = img.shape[1]
+    # width = 540
+    # height = int(float(width) / col * row)
+    # img = cv2.resize(img, (width, height))
     vhs = HandleImage(img)
     # cv2.imwrite(output, vhs)
     cv2.imshow('output', vhs)
@@ -174,18 +151,17 @@ def doWithImage(src, output = 'out.jpg'):
 
 if __name__ == "__main__":
     # doWithImage('test.jpg')
-    
-    username = "jam"
+    # pass
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", type = str, default = '/Users/{}/Desktop/input.mp4'.format(username))
-    parser.add_argument("-o", "--output", type = str, default = "/Users/{}/Desktop/output.mp4".format(username))
-    parser.add_argument("-height", "--perferheight", type = int, default = 480)
-    parser.add_argument("-fps", "--framepersecond", type = int, default = 0)
+    parser.add_argument("-i", "--input", type = str, default = '/Users/zjj/Downloads/test.mp4')
+    parser.add_argument("-o", "--output", type = str, default = '/Users/zjj/Downloads/C0105.out.mov')
+    parser.add_argument("-height", "--perferheight", type = int, default = 720)
+    parser.add_argument("-fps", "--framepersecond", type = int, default = 30)
     parser.add_argument("-x264", "--encode264", type = int, default = 1)
-    parser.add_argument("-interlaced", "--interlaced", type = int, default = 1)
+    parser.add_argument("-interlaced", "--interlaced", type = int, default = 0)
     args = vars(parser.parse_args())
 
     print(args)
 
-    doWithVideo(args["input"], output = args["output"], encodewith264 = (args["encode264"] > 0), perferheight = args["perferheight"], framepersecond = args["framepersecond"], interlaced = args["interlaced"])
+    doWithVideo(args["input"], output = args["output"], encodewith264 = args["encode264"], perferheight = args["perferheight"], framepersecond = args["framepersecond"], interlaced = args["interlaced"])
